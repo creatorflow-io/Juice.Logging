cfbe222835f1b92a8b7eb0d223b9158bff8fa1eb
ductran <duc@hdvietnam.com.vn>
2023-12-11 18:47:19 +0700

    Implement scope events for SignalRProvider

--- src/Juice.Extensions.Logging.File/Extensions/Logging/File/LogEntry.cs
+++ src/Juice.Extensions.Logging.File/Extensions/Logging/File/LogEntry.cs
@@ -18,25 +18,6 @@ namespace Juice.Extensions.Logging.File
         public string Category { get; init; }
         public LogLevel LogLevel { get; init; }
         public Exception? Exception { get; protected set; }
-        public List<LogScope>? Scopes { get; protected set; }
-        public void PushScope(LogScope scope)
-        {
-            if (Scopes == null)
-            {
-                Scopes = new List<LogScope>();
-            }
-            Scopes.Add(scope);
-        }
-        public List<string> GetScopes()
-        {
-            return Scopes?.Where(s => !string.IsNullOrEmpty(s.Scope))
-                 .Select(s => s.Scope!)?.ToList() ?? new List<string>();
-        }
     }
 
-    internal class LogScope
-    {
-        public string? Scope { get; init; }
-        public Dictionary<string, object>? Properties { get; set; }
-    }
 }
--- test/Juice.Extensions.Logging.Tests.Host/LogHub.cs
+++ test/Juice.Extensions.Logging.Tests.Host/LogHub.cs
@@ -5,13 +5,21 @@ namespace Juice.Extensions.Logging.Tests.Host
 {
     public class LogHub : Hub<ILogClient>
     {
-        public async Task LoggingAsync(Guid serviceId, string? traceId, string category, string message, LogLevel level, string? contextual, string[] scopes)
+        public async Task LoggingAsync(Guid serviceId, string? traceId, string category, string message, LogLevel level, string? contextual, object[] scopes)
         {
             await Clients.Others.LoggingAsync(serviceId, traceId, category, message, level, contextual, scopes);
         }
         public async Task StateAsync(Guid serviceId, string? traceId, string state, string message)
         {
             await Clients.Others.StateAsync(serviceId, traceId, state, message);
+        }
+        public async Task BeginScopeAsync(Guid serviceId, string? traceId, string category, object scope)
+        {
+            await Clients.Others.BeginScopeAsync(serviceId, traceId, category, scope);
+        }
+        public async Task EndScopeAsync(Guid serviceId, string? traceId, string category, object scope)
+        {
+            await Clients.Others.EndScopeAsync(serviceId, traceId, category, scope);
         }
     }
 }
