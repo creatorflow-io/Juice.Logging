4967c772f872b3c4e5c13bf59cba5d079bdcf56b
ductran <duc@hdvietnam.com.vn>
2023-12-29 14:10:06 +0700

    Add gRPC logger

---
+++ src/Juice.Extensions.Logging.Grpc/Extensions/Logging/Grpc/GrpcLoggerBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Juice.Extensions.Logging.Grpc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace Juice.Extensions.Logging
+{
+    public static class GrpcLoggerBuilderExtensions
+    {
+        public static ILoggingBuilder AddGrpcLogger(this ILoggingBuilder builder, IConfigurationSection configuration)
+        {
+            builder.Services.Configure<GrpcLoggerOptions>(configuration);
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, GrpcLoggerProvider>());
+
+            return builder;
+        }
+
+        public static ILoggingBuilder AddGrpcLogger(this ILoggingBuilder builder, Action<GrpcLoggerOptions> configure)
+        {
+            builder.Services.Configure(configure);
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, GrpcLoggerProvider>());
+
+            return builder;
+        }
+    }
+}
--- src/Juice.Extensions.Logging.Grpc/Extensions/Logging/Grpc/GrpcLoggerProvider.cs
+++ src/Juice.Extensions.Logging.Grpc/Extensions/Logging/Grpc/GrpcLoggerProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Concurrent;
-using Juice.Extensions.Logging.EF.LogEntries;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
 using Juice.MultiTenant;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,26 +8,28 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 
-namespace Juice.Extensions.Logging.EF
+namespace Juice.Extensions.Logging.Grpc
 {
-    [ProviderAlias("Db")]
-    internal class EFLoggerProvider : LoggerProvider
+    [ProviderAlias("Grpc")]
+    internal class GrpcLoggerProvider : LoggerProvider
     {
-        private ConcurrentQueue<LogEntry> _logQueue = new();
+        private ConcurrentQueue<GrpcLogEntry> _logQueue = new();
         private TimeSpan _bufferTime = TimeSpan.FromSeconds(3);
-        private IOptionsMonitor<EFLoggerOptions> _optionsMonitor;
         private ILogger _logger;
-        private IServiceScopeFactory _serviceScopeFactory;
+        private IOptionsMonitor<GrpcLoggerOptions> _optionsMonitor;
         private IHttpContextAccessor? _httpContextAccessor;
+        private IServiceScopeFactory _serviceScopeFactory;
+        private GrpcChannel? _channel;
 
-        public EFLoggerProvider(IServiceScopeFactory serviceScopeFactory,
-            IOptionsMonitor<EFLoggerOptions> optionsMonitor,
+        public GrpcLoggerProvider(
+            IOptionsMonitor<GrpcLoggerOptions> optionsMonitor,
+            IServiceScopeFactory serviceScopeFactory,
             IHttpContextAccessor? httpContextAccessor = default
             )
         {
-            _optionsMonitor = optionsMonitor;
             _serviceScopeFactory = serviceScopeFactory;
             _httpContextAccessor = httpContextAccessor;
+            _optionsMonitor = optionsMonitor;
             _optionsMonitor.OnChange((options, _) =>
             {
                 if (options.Disabled)
@@ -37,22 +40,34 @@ namespace Juice.Extensions.Logging.EF
                 }
                 else if (_backgroundTask == null)
                 {
+                    _bufferTime = options.BufferTime;
                     _shutdown = !_shutdown.IsCancellationRequested ? _shutdown : new CancellationTokenSource();
                     _backgroundTask = Task.Run(ExecuteAsync);
                 }
             });
-            _logger = IsolatedLoggerHelper.BuildLogger("DBLogger", _optionsMonitor.CurrentValue);
+            _logger = IsolatedLoggerHelper.BuildLogger("GrpcLogger", _optionsMonitor.CurrentValue);
             _bufferTime = _optionsMonitor.CurrentValue.BufferTime;
-
+            var endpoint = _optionsMonitor.CurrentValue.Endpoint;
+            if (string.IsNullOrEmpty(endpoint))
+            {
+                _logger.LogWarning("Grpc endpoint is not configured. Grpc logger is disabled.");
+            }
+            else
+            {
+                _channel = GrpcChannel.ForAddress(new Uri(endpoint));
+            }
             if (!_optionsMonitor.CurrentValue.Disabled)
             {
                 _backgroundTask = Task.Run(ExecuteAsync);
             }
         }
 
+        private bool IsEnabled => _channel != null
+            && _backgroundTask != null && !_backgroundTask.IsCompleted;
+
         public override void WriteLog<TState>(LogEntry<TState> entry, string formattedMessage, IExternalScopeProvider? scopeProvider)
         {
-            if (_backgroundTask != null && !_backgroundTask.IsCompleted)
+            if (IsEnabled)
             {
                 Guid? serviceId = default;
                 string? traceId = default;
@@ -86,17 +101,40 @@ namespace Juice.Extensions.Logging.EF
                     {
                         using var scope = _serviceScopeFactory.CreateScope();
                         var tenantId = scope.ServiceProvider.GetService<ITenant>()?.Id ?? "";
-                        _logQueue.Enqueue(new LogEntry(serviceId, traceId, operation, entry.Category, formattedMessage, entry.LogLevel, entry.Exception?.StackTrace, tenantId));
+                        _logQueue.Enqueue(new GrpcLogEntry
+                        {
+                            ServiceId = serviceId?.ToString(),
+                            TraceId = traceId,
+                            Operation = operation,
+                            Category = entry.Category,
+                            Message = formattedMessage,
+                            Level = (int)entry.LogLevel,
+                            Exception = entry.Exception?.StackTrace,
+                            TenantId = tenantId,
+                            Timestamp = Timestamp.FromDateTimeOffset(DateTimeOffset.UtcNow)
+                        });
                     }
                 }
                 else
                 {
                     traceId = traceId ?? _httpContextAccessor!.HttpContext!.TraceIdentifier;
                     var tenantId = serviceProvider.GetService<ITenant>()?.Id ?? "";
-                    _logQueue.Enqueue(new LogEntry(serviceId, traceId, operation, entry.Category, formattedMessage, entry.LogLevel, entry.Exception?.StackTrace, tenantId));
+                    _logQueue.Enqueue(new GrpcLogEntry
+                    {
+                        ServiceId = serviceId?.ToString(),
+                        TraceId = traceId,
+                        Operation = operation,
+                        Category = entry.Category,
+                        Message = formattedMessage,
+                        Level = (int)entry.LogLevel,
+                        Exception = entry.Exception?.StackTrace,
+                        TenantId = tenantId,
+                        Timestamp = Timestamp.FromDateTimeOffset(DateTimeOffset.UtcNow)
+                    });
                 }
             }
         }
+
 
         #region Service
 
@@ -118,6 +156,7 @@ namespace Juice.Extensions.Logging.EF
                     await Task.Delay(_bufferTime, _shutdown.Token);
                 }
                 catch (TaskCanceledException) { }
+
                 await WriteFromQueueAsync();
             }
 
@@ -125,27 +164,28 @@ namespace Juice.Extensions.Logging.EF
         }
 
         /// <summary>
-        /// Dequeue and write log entry to DB
+        /// Dequeue and write log entry to gRPC
         /// </summary>
         private async Task WriteFromQueueAsync()
         {
-            var logs = new List<LogEntry>();
+            var logs = new List<GrpcLogEntry>();
             while (_logQueue.TryDequeue(out var log))
             {
                 logs.Add(log);
             }
-            if (logs.Any())
+            if (logs.Any() && _channel != null)
             {
                 try
                 {
-                    using var scope = _serviceScopeFactory.CreateScope();
-                    var dbContext = scope.ServiceProvider.GetRequiredService<LogDbContext>();
-                    dbContext.Logs.AddRange(logs);
-                    await dbContext.SaveChangesAsync();
+                    var client = new LogWriter.LogWriterClient(_channel);
+                    await client.LogAsync(new LogEntries
+                    {
+                        Entries = { logs }
+                    });
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError(ex, "Error while writing logs to database");
+                    _logger.LogError(ex, "Error while writing logs to grpc");
                 }
             }
         }
