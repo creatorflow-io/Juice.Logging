23e980527fd0e359632b5ebb2a7858ffeaf83ba4
ductran <ductran.tb@gmail.com>
2024-06-13 17:44:53 +0700

    Test loghub without scopes

--- test/Juice.Extensions.Logging.Tests.Host/LogHub.cs
+++ test/Juice.Extensions.Logging.Tests.Host/LogHub.cs
@@ -3,12 +3,18 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Juice.Extensions.Logging.Tests.Host
 {
-    public class LogHub : Hub<ILogClient>
+    public class LogHub : Hub<ILogClient1>
     {
         public async Task LoggingAsync(Guid serviceId, string? traceId, string category, string message, LogLevel level, string? contextual, object[] scopes)
         {
             await Clients.Others.LoggingAsync(serviceId, traceId, category, message, level, contextual, scopes);
         }
+
+        //public async Task LoggingAsync(Guid senderId, string jobId, string message, LogLevel level, string? contextual)
+        //{
+        //    await Clients.Others.LoggingAsync(senderId, jobId, message, level, contextual);
+        //}
+
         public async Task StateAsync(Guid serviceId, string? traceId, string state, string message)
         {
             await Clients.Others.StateAsync(serviceId, traceId, state, message);
@@ -21,5 +27,9 @@ namespace Juice.Extensions.Logging.Tests.Host
         {
             await Clients.Others.EndScopeAsync(serviceId, traceId, category, scope);
         }
+    }
+    public interface ILogClient1: ILogClient
+    {
+        Task LoggingAsync(Guid senderId, string jobId, string message, LogLevel level, string? contextual);
     }
 }
